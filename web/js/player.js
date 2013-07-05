2d28cee277c08afe5dbcb37029369fad21944e0b
lroth <lukasz.roth@gmail.com>
2013-07-05 22:59:54 +0200

    fixed drop

--- web/js/player.js
+++ web/js/player.js
@@ -21,26 +21,28 @@
         socket.on('update_teams', function(data) {
             console.log(data);
             _.each(data.teams, function(players, team) {
-
                 _.each(players, function(player) {
                     $('ul.players').find('li:contains(' + player + ')').appendTo(teams[team]);
                 });
             });
             //update view
+
+            //redirect if ready
         });
 
         //drag and drop players
         $( "ul.players, ul.player-placeholder" ).sortable({
           connectWith: "ul.player-placeholder, ul.players",
           receive: function( event, ui ) {
-            var name = $(this).children('li').html();
+
+            var name = ui.item.html();
             var team = $(this).parent().children("ul").attr('id');
 
             socket.emit('choose_player', { 'name': name, 'team': team });
           }
         }).disableSelection();
 
-
+        // randomize players
         $('#random').on('click', function (event) {
             //reset all selected players
             teams.firstTeam.find('li').appendTo(players);
