1e09fdb8667946e54c6d52a2bbd8bd99dddb5744
cypherq <cypherq@gmail.com>
2013-07-05 18:23:17 +0200

    merging Chris changes

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 /* start application */
 var express = require('express');
 
-/* require routing and views */
-var routing = require('./routing');
-var views 	= require('./views');
-
-/* start express application */
+/* start app */
 var app = express();
 
-console.log(routing);
+/* create http server for sockets */
+var socketServer = require('http').createServer(app);
+
+/* socket.io stuff */
+var io = require('socket.io').listen(socketServer);
+
+/* require routing and views */
+var routing = require('./routing')(app, io);
+
+/* start express application */
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+/* tell me where is statik path */
+app.use(express.static(__dirname + '/web/'));
+
+/* use session stuff */
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
+
+/* start listening */
+socketServer.listen(1337);
--- routing.js
+++ routing.js
@@ -1,3 +1,31 @@
-module.exports = {
-  test : 'routing'
-};
+function routes(app, io) {
+	app.get('/', function (req, res) {
+	  res.render('index', {});
+	});
+
+	app.get('/judge', function (req, res) {
+	  res.render('judge', {});
+	});
+
+	app.get('/player', function(req, res) {
+		var clientsNo = [
+		    'player1',
+		    'player2',
+		    'player3',
+		    'player4'
+		];
+
+	  res.send('Your Awesome.');
+	  io.sockets.on('connection', function (socket) {
+	    // console.log(socket);
+	    socket.emit('players', { players: clientsNo });
+	    // socket.on('my other event', function (data) {
+	    //   console.log(data);
+	    // });
+	  });
+	});
+
+	return {};
+}
+
+module.exports = routes;
