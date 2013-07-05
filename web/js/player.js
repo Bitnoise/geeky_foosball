c97f340dcc2ea30041fede02142150c87f538b6e
lroth <lukasz.roth@gmail.com>
2013-07-05 21:18:25 +0200

    drag and drop

--- web/js/player.js
+++ web/js/player.js
@@ -1,7 +1,21 @@
 (function($){
-    $('ul.players li').on('click', function(event) {
-        // console.log($(this).html());
-        var socket = io.connect('http://localhost:1337');
-        socket.emit('choose_player', { my: $(this).html() });
-    });
+    $(document).ready(function() {
+
+        $( "ul.players, ul.player-placeholder" ).sortable({
+          connectWith: "ul.player-placeholder, ul.players",
+          receive: function( event, ui ) {
+            var name = $(this).children('li').html();
+            var team = $(this).parent().children("ul").attr('id');
+
+            var socket = io.connect('http://localhost:1337');
+            socket.emit('choose_player', { 'name': name, 'team': team });
+          }
+        }).disableSelection();
+
+        $('ul.players li').on('click', function(event) {
+            // console.log($(this).html());
+            // var socket = io.connect('http://localhost:1337');
+            // socket.emit('choose_player', { my: $(this).html() });
+        });
+    })
 })(jQuery)
