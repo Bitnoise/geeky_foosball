5a549c69dfef0f68c6de2d16420e8442d2cc80d7
Michał Soczyński <soczynski.michal@gmail.com>
2013-07-05 22:03:32 +0200

    add random

--- web/js/player.js
+++ web/js/player.js
@@ -1,5 +1,13 @@
+
 (function($){
+    function shuffle(o){ //v1.0
+        for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    }
     $(document).ready(function() {
+        var firstTeam  = $('#first-team');
+        var secondTeam = $('#second-team');
+        var players    = $('#players');
 
         $( "ul.players, ul.player-placeholder" ).sortable({
           connectWith: "ul.player-placeholder, ul.players",
@@ -16,6 +24,21 @@
             // console.log($(this).html());
             // var socket = io.connect('http://localhost:1337');
             // socket.emit('choose_player', { my: $(this).html() });
+        });
+
+        $('#random').on('click', function (event) {
+            var arr   = shuffle(players.find('li')); //shuffle players
+
+            //reset all selected players
+            firstTeam.find('li').appendTo(players);
+            secondTeam.find('li').appendTo(players);
+
+            for (var i=0; i<4; i++)
+            {
+                (i%2 ? firstTeam.append(arr[i]) : secondTeam.append(arr[i]));
+            }
+
+            return false;
         });
     })
 })(jQuery)
