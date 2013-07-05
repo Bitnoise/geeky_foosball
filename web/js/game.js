3e2ad99e62724745848d1aa5e361e416b24d8351
Michał Soczyński <soczynski.michal@gmail.com>
2013-07-05 22:52:32 +0200

    end goal

--- web/js/game.js
+++ web/js/game.js
@@ -1,7 +1,14 @@
 (function($){
     $(document).ready(function() {
+        var firstTeam  = $('#firstTeam');
+        var secondTeam = $('#secondTeam');
+
+        socket.on('scoreChange', function (data) {
+            firstTeam.html(data.firstTeam);
+            secondTeam.html(data.secondTeam);
+        });
         $('#goal').on('click', function(event) {
-            socket.emit('goal', {
+            socket.emit('scoreChange', {
                 'name': 'Michał'
             });
         });
